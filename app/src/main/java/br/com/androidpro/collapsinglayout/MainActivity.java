eb7cf2be2607ae7516a752eaa9bb6075f8dbddd8
Fillipe Cordeiro <fillipe@androidpro.com.br>
2017-10-06 13:15:42 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/androidpro/collapsinglayout/MainActivity.java
@@ -0,0 +1,30 @@
+package br.com.androidpro.collapsinglayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
